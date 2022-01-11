5293a59ae884295cc624011600b127375fea381c
DaniilKorzun <korzundanik@gmail.com>
2022-01-12 02:08:25 +0300

    Created RESTController and tested POST method for saving Transactions from file

--- src/main/java/com/springproject/fileparserwebapp/controllers/FileUploadController.java
+++ src/main/java/com/springproject/fileparserwebapp/controllers/FileUploadController.java
@@ -1,10 +1,13 @@
 package com.springproject.fileparserwebapp.controllers;
 
+import com.springproject.fileparserwebapp.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,23 +15,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Controller
+@RestController
 public class FileUploadController {
-    @Value("${upload.path}")
-    private String uploadPath;
+    private final TransactionService transactionService;
 
-    @GetMapping("/file_upload")
-    public String startPage() {
-        return "upload_file";
+    @Value("${upload.path}")
+    private String UPLOAD_PATH;
+
+    public FileUploadController(TransactionService transactionService) {
+        this.transactionService = transactionService;
     }
 
     @PostMapping("/file_upload")
-    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         if (file != null) {
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(uploadPath + file.getOriginalFilename());
+            Path path = Paths.get(UPLOAD_PATH + file.getOriginalFilename());
             Files.write(path, bytes);
+            transactionService.parseFileAndSaveTransactions(path.toFile());
         }
-        return "upload_file";
+        return ResponseEntity.ok("File is parsed and transactions are saved");
     }
 }
