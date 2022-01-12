0fc5fc3e23894e25b0b6379c561ebe35e1d9a3e1
DaniilKorzun <korzundanik@gmail.com>
2022-01-12 22:01:13 +0300

    Created User entity and added validation to FileUploadService

--- src/main/java/com/springproject/fileparserwebapp/services/FileUploadService.java
+++ src/main/java/com/springproject/fileparserwebapp/services/FileUploadService.java
@@ -1,5 +1,6 @@
 package com.springproject.fileparserwebapp.services;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,23 +9,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class FileUploadService {
     @Value("${upload.path}")
     private String UPLOAD_PATH;
+    private final Set<String> ALLOWED_FILE_EXTENSIONS = new HashSet<>(Arrays.asList("xml", "csv"));
 
     public boolean uploadFiles(MultipartFile ... files) {
+        if (isAllowedFilesUploaded(files)) {
+            for (MultipartFile file : files) {
+                writeFile(file);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isAllowedFilesUploaded(MultipartFile ... files) {
         for (MultipartFile file : files) {
-            try {
-                byte[] bytes = file.getBytes();
-                Path path = Paths.get(UPLOAD_PATH + "/" + file.getOriginalFilename());
-                Files.write(path, bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!isAllowedExtension(file.getOriginalFilename())) {
+                return false;
             }
         }
         return true;
+    }
+
+    private boolean isAllowedExtension(String filename) {
+        return ALLOWED_FILE_EXTENSIONS.contains(FilenameUtils.getExtension(filename));
+    }
+
+    private void writeFile(MultipartFile file) {
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_PATH + "/" + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
