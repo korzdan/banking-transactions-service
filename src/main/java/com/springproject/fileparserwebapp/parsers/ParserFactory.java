1c6a6457b6be786c6b17b713fc68c4fe6293be1e
DaniilKorzun <korzundanik@gmail.com>
2022-02-07 20:07:07 +0300

    Edit ParserFactory with usage of Reflection API

--- src/main/java/com/springproject/fileparserwebapp/parsers/ParserFactory.java
+++ src/main/java/com/springproject/fileparserwebapp/parsers/ParserFactory.java
@@ -4,19 +4,30 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class ParserFactory {
-    private static final String XML_EXTENSION = "xml";
-    private static final String CSV_EXTENSION = "csv";
+    private static final Map<String, Class<? extends Parser>> availableParser = new HashMap<>();
+
+    static {
+        availableParser.put("xml", XMLParser.class);
+        availableParser.put("csv", CSVParser.class);
+    }
 
     public Parser createParser(MultipartFile file) {
-        String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
-        if (fileExtension.equals(XML_EXTENSION)) {
-            return new XMLParser();
+        try {
+            String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+            Class<?> theClass = availableParser.get(fileExtension);
+            Constructor parserConstructor = theClass.getDeclaredConstructor();
+            return (Parser) parserConstructor.newInstance(new Object[] {});
+        } catch (Exception e) {
+            // TODO: Handle the exception in the right way
+            // Right now this is a stub
+            e.printStackTrace();
         }
-        if (fileExtension.equals(CSV_EXTENSION)) {
-            return new CSVParser();
-        }
-        throw new RuntimeException("Parser is not created");
+        throw new RuntimeException();
     }
 }
