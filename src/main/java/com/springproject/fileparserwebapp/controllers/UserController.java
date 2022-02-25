448199c42c83505a5407c799375709ffc97de550
DaniilKorzun <korzundanik@gmail.com>
2022-02-25 16:27:37 +0300

    Add getAllTransactionsOfUser method to UserController

--- src/main/java/com/springproject/fileparserwebapp/controllers/UserController.java
+++ src/main/java/com/springproject/fileparserwebapp/controllers/UserController.java
@@ -7,20 +7,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping
     @PreAuthorize("hasAuthority('execute_command')")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/transactions")
+    public ResponseEntity<?> getAllTransactionsOfUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getTransactionsOfUser(id), HttpStatus.OK);
     }
 
 }
