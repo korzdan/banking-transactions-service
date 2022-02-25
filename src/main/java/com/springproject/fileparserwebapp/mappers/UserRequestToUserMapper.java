9b8df7ec2952c893b90ed3eba611ff7d04a7de11
DaniilKorzun <korzundanik@gmail.com>
2022-02-25 19:06:14 +0300

    Create UserRequestToUserMapper

---
+++ src/main/java/com/springproject/fileparserwebapp/mappers/UserRequestToUserMapper.java
@@ -0,0 +1,15 @@
+package com.springproject.fileparserwebapp.mappers;
+
+import com.springproject.fileparserwebapp.dto.UserRequestDTO;
+import com.springproject.fileparserwebapp.models.User;
+
+public class UserRequestToUserMapper {
+    public static User mapUserRequestDTOToUser(UserRequestDTO userDto) {
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setRole(userDto.getRole());
+        user.setStatus(userDto.getStatus());
+        return user;
+    }
+}
