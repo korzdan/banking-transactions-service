0d43ca51a83df61cd6731f5a78bb9f473fcdd916
DaniilKorzun <korzundanik@gmail.com>
2022-02-26 00:06:39 +0300

    Add method for user registration in UserController

--- src/main/java/com/springproject/fileparserwebapp/controllers/UserController.java
+++ src/main/java/com/springproject/fileparserwebapp/controllers/UserController.java
@@ -1,26 +1,36 @@
 package com.springproject.fileparserwebapp.controllers;
 
+import com.springproject.fileparserwebapp.dto.UserRequestDTO;
 import com.springproject.fileparserwebapp.models.User;
 import com.springproject.fileparserwebapp.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasAuthority('execute_command')")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    @GetMapping("/users")
-    @PreAuthorize("hasAuthority('execute_command')")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRequestDTO userDTO) {
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        userService.registerUser(userDTO);
+        return new ResponseEntity<>("User has been successfully registered!", HttpStatus.OK);
     }
 
 }
