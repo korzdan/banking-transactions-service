33a771a4758191e933c3db71e130061cbccc238f
DaniilKorzun <korzundanik@gmail.com>
2022-02-26 00:05:34 +0300

    Refactor UserRequestDTO

--- src/main/java/com/springproject/fileparserwebapp/dto/UserRequestDTO.java
+++ src/main/java/com/springproject/fileparserwebapp/dto/UserRequestDTO.java
@@ -1,7 +1,6 @@
 package com.springproject.fileparserwebapp.dto;
 
 import com.springproject.fileparserwebapp.models.Role;
-import com.springproject.fileparserwebapp.models.Status;
 import lombok.Data;
 
 @Data
@@ -9,5 +8,4 @@ public class UserRequestDTO {
     private String username;
     private String password;
     private Role role;
-    private Status status;
 }
