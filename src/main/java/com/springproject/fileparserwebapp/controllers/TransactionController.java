50c7ed07b0ea508fadeb2c02cc33897be5c0819a
DaniilKorzun <korzundanik@gmail.com>
2022-02-25 13:45:57 +0300

    Add getTransaction method in TransactionController

--- src/main/java/com/springproject/fileparserwebapp/controllers/TransactionController.java
+++ src/main/java/com/springproject/fileparserwebapp/controllers/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class TransactionController {
     @GetMapping
     public ResponseEntity<?> getAllTransactions() {
         return new ResponseEntity<>(transactionService.findAllTransactions(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTransaction(@PathVariable Long id) {
+        return new ResponseEntity<>(transactionService.findTransactionById(id), HttpStatus.OK);
     }
 
     @GetMapping("/top")
