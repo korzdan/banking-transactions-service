2ecbe30cd61ce3a235e42d665f04d86f8f15f32e
DaniilKorzun <korzundanik@gmail.com>
2022-02-25 18:42:34 +0300

    Create UserDTO

---
+++ src/main/java/com/springproject/fileparserwebapp/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.springproject.fileparserwebapp.dto;
+
+import com.springproject.fileparserwebapp.models.Role;
+import com.springproject.fileparserwebapp.models.Status;
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String username;
+    private String password;
+    private Role role;
+    private Status status;
+}
