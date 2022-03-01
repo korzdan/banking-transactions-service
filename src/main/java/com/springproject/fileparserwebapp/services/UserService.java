086bfc83c4bfab5992ea9a7837737da9bff8e193
DaniilKorzun <korzundanik@gmail.com>
2022-03-02 00:05:24 +0300

    Fix UserService

--- src/main/java/com/springproject/fileparserwebapp/services/UserService.java
+++ src/main/java/com/springproject/fileparserwebapp/services/UserService.java
@@ -1,10 +1,14 @@
 package com.springproject.fileparserwebapp.services;
 
+import com.springproject.fileparserwebapp.dto.UserRequestDTO;
 import com.springproject.fileparserwebapp.exception.UserNotFound;
+import com.springproject.fileparserwebapp.exception.UsernameAlreadyExists;
+import com.springproject.fileparserwebapp.models.Status;
 import com.springproject.fileparserwebapp.models.Transaction;
 import com.springproject.fileparserwebapp.models.User;
 import com.springproject.fileparserwebapp.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,6 +21,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper = new ModelMapper();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -36,5 +41,23 @@ public class UserService implements UserDetailsService {
 
     public List<Transaction> getTransactionsOfUser(Long id) throws UserNotFound {
         return new ArrayList<>(findUserById(id).getTransactions());
+    }
+
+    public User registerUser(UserRequestDTO userDto) {
+        if (isExistByUsername(userDto.getUsername())) {
+            throw new UsernameAlreadyExists("This username is already taken.");
+        } else {
+            return userRepository.save(mapUserRequestDtoToUser(userDto));
+        }
+    }
+
+    private boolean isExistByUsername(String username) {
+        return userRepository.findByUsername(username).isPresent();
+    }
+
+    private User mapUserRequestDtoToUser(UserRequestDTO userDto) {
+        User newUser = modelMapper.map(userDto, User.class);
+        newUser.setStatus(Status.ACTIVE);
+        return newUser;
     }
 }
