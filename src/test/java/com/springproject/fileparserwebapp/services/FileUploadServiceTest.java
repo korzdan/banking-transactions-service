71655d1b07c527adfbfdafeea5be67f8cd40dd3b
DaniilKorzun <korzundanik@gmail.com>
2022-03-03 19:40:31 +0300

    Refactor and add new tests in FileUploadServiceTest

--- src/test/java/com/springproject/fileparserwebapp/services/FileUploadServiceTest.java
+++ src/test/java/com/springproject/fileparserwebapp/services/FileUploadServiceTest.java
@@ -1,45 +1,59 @@
 package com.springproject.fileparserwebapp.services;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FileUploadServiceTest {
 
-    @Autowired
-    private FileUploadService fileUploadService;
+    private FileUploadService fileUploadService = new FileUploadService();
     private File xmlFile;
     private File csvFile;
-    private File docxFile;
+    private File incorrectFile;
+
+    private MockMultipartFile xmlMultipart;
+    private MockMultipartFile csvMultipart;
+    private MockMultipartFile txtMultipart;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
         xmlFile = new File("D:\\Internship - ITechArtRep\\Spring Project Info\\FilesExample\\xml_example.xml");
-        csvFile = new File("D:\\Internship - ITechArtRep\\Spring Project Info\\Spring Project.docx");
-        docxFile = new File("D:\\Internship - ITechArtRep\\Spring Project Info\\FilesExample\\csv_example.csv");
+        csvFile = new File("D:\\Internship - ITechArtRep\\Spring Project Info\\FilesExample\\csv_example.csv");
+        incorrectFile = new File("D:\\Internship - ITechArtRep\\Spring Project Info\\FilesExample\\file.txt");
+
+        xmlMultipart = new MockMultipartFile("file.xml",
+                "xml_example.xml", "text/xml", new FileInputStream(xmlFile));
+        csvMultipart = new MockMultipartFile("file.csv",
+                "csv_example.csv", "text/csv", new FileInputStream(csvFile));
+        txtMultipart = new MockMultipartFile("text.txt", "file.txt",
+                "text/plain", new FileInputStream(incorrectFile));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"file.xml", "text.csv"})
+    @DisplayName("Return true for files that can be parsed")
+    void ReturnTrue_ForFilesThatCanBeParsed(String input) {
+        assertEquals(true,
+                ReflectionTestUtils.invokeMethod(fileUploadService, "isAllowedExtension", input));
     }
 
     @Test
-    void Return3_WhenAllowedFilesUploadedAndAllFilesReturned() throws IOException {
-        MultipartFile firstMultipartFile = new MockMultipartFile("first.xml", "xml_example.xml",
-                "text/xml", new FileInputStream(xmlFile));
-        MultipartFile secondMultipartFile = new MockMultipartFile("second.docx", "Spring Project.docx",
-                "application/vnd.openxmlformats-officedocument.wordprocessingml.document", new FileInputStream(csvFile));
-        MultipartFile thirdMultipartFile = new MockMultipartFile("third.csv",
-                "csv_example.csv", "text/csv", new FileInputStream(docxFile));
-
-        List<MultipartFile> allowedFiles = fileUploadService.uploadAllowedFiles(firstMultipartFile,
-                secondMultipartFile, thirdMultipartFile);
-        Assertions.assertEquals(3, allowedFiles.size());
+    @DisplayName("Return List<MultipartFile> that are passed to uploadAllowedFiles(...)")
+    void ReturnListOfPassedFiles_WhenCallingUploadAllowedFiles() {
+        ReflectionTestUtils.setField(fileUploadService, "UPLOAD_FILE_DIR",
+                "D:/Internship - ITechArtRep/Internship - Spring Boot project/src/main/resources/uploadedFiles");
+        List<MultipartFile> files = List.of(xmlMultipart, csvMultipart, txtMultipart);
+        assertEquals(files, fileUploadService.uploadAllowedFiles(xmlMultipart, csvMultipart, txtMultipart));
     }
 }
