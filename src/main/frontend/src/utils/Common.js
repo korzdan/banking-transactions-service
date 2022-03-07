a6f5e29ea9444d9df7e6e2d18166dbedfb5c338d
DaniilKorzun <korzundanik@gmail.com>
2022-03-07 15:13:41 +0300

    Add method for getting role from token

--- src/main/frontend/src/utils/Common.js
+++ src/main/frontend/src/utils/Common.js
@@ -1,7 +1,13 @@
+import jwt from 'jwt-decode';
+
 export const getToken = () => {
     return localStorage.getItem("token") || null;
 }
 
 export const setToken = (token) => {
     localStorage.setItem("token", token);
+}
+
+export const getRole = (token) => {
+    return jwt(token).role;
 }
