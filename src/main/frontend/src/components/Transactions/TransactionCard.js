e00903178852487cf27e85866fa3ab4abc8cd6d6
DaniilKorzun <korzundanik@gmail.com>
2022-03-08 01:17:22 +0300

    Create TransactionCard component and add styles to it

---
+++ src/main/frontend/src/components/Transactions/TransactionCard.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {getToken} from "../../utils/Common";
+import "../Transactions/TransactionCard.css"
+
+const TransactionCard = (props) => {
+
+    const [transaction, setTransaction] = useState([]);
+
+    useEffect(() => {
+        axios.get(props.url, {
+            headers : {
+                'Authorization' : getToken()
+            }
+        }).then(response => {
+            setTransaction(response.data);
+        });
+    }, []);
+
+    return (
+        <div className="outer-card">
+            <div className="transaction-card">
+                <div className="card-field">Transaction ID: {transaction.transactionId}</div>
+                <div className="card-field">User ID: {transaction.userId}</div>
+                <div className="card-field">Status: {transaction.status}</div>
+                <div className="card-field">Amount: {transaction.amount}</div>
+                <div className="card-field">Currency: {transaction.currency}</div>
+                <div className="card-field">Timestamp: {transaction.timestamp}</div>
+            </div>
+        </div>
+    );
+};
+
+export default TransactionCard;
