df55d041b5d451bdaf65438368304eed1fa89900
DaniilKorzun <korzundanik@gmail.com>
2022-03-11 00:07:06 +0300

    Style FoundTransaction component

--- src/main/frontend/src/components/FoundTransaction/FoundTransaction.js
+++ src/main/frontend/src/components/FoundTransaction/FoundTransaction.js
@@ -8,6 +8,7 @@ const FoundTransaction = () => {
 
     const {id} = useParams();
     const [transaction, setTransaction] = useState('');
+    const [error, setError] = useState('');
 
     useEffect(() => {
         axios.get(`http://localhost:8080/transactions/${id}`, {
@@ -16,19 +17,26 @@ const FoundTransaction = () => {
             }
         }).then(response => {
             setTransaction(response.data);
+        }).catch(error => {
+            if (error.response.status === 404) {
+                setError("No transaction has been found.");
+            }
         });
     }, []);
 
     return (
-        <div className="outer-card">
-            <div className="transaction-card">
-                <div className="card-field">Transaction ID: {transaction.transactionId}</div>
-                <div className="card-field">User ID: {transaction.userId}</div>
-                <div className="card-field">Status: {transaction.status}</div>
-                <div className="card-field">Amount: {transaction.amount}</div>
-                <div className="card-field">Currency: {transaction.currency}</div>
-                <div className="card-field">Timestamp: {transaction.timestamp}</div>
-            </div>
+        <div>
+            {error !== '' && <div className="search-error">{error}</div>}
+            {transaction.transactionId !== undefined && <div className="outer-card">
+                <div className="transaction-card">
+                    <div className="card-field">Transaction ID: {transaction.transactionId}</div>
+                    <div className="card-field">User ID: {transaction.userId}</div>
+                    <div className="card-field">Status: {transaction.status}</div>
+                    <div className="card-field">Amount: {transaction.amount}</div>
+                    <div className="card-field">Currency: {transaction.currency}</div>
+                    <div className="card-field">Timestamp: {transaction.timestamp}</div>
+                </div>
+            </div>}
         </div>
     );
 };
