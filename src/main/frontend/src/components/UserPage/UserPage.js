69edc93b87380f57ed881ce4d074f6c7b91dde8c
DaniilKorzun <korzundanik@gmail.com>
2022-03-11 01:43:18 +0300

    Style UserPage component

--- src/main/frontend/src/components/UserPage/UserPage.js
+++ src/main/frontend/src/components/UserPage/UserPage.js
@@ -14,26 +14,32 @@ const UserPage = () => {
 
     return (
         <div className="user-page-outer">
-            <div className="username">{user.username}</div>
-            <div className="user-role">{user.role}</div>
-            <div className="user-status">{user.status}</div>
-            <div className="user-transactions">
-                <Link to={{
-                    pathname: `${user.id}/transactions`,
-                    state: user.transactions
-                }}>Transactions</Link>
-            </div>
-            <div className="user-files">
-                <Link to={{
-                    pathname: `${user.id}/files`,
-                    state: user.files
-                }}>Files</Link>
-            </div>
-            <div className="user-errors">
-                <Link to={{
-                    pathname: `${user.id}/errors`,
-                    state: user.errors
-                }}>Errors</Link>
+            <div className="user-page-inner">
+                <div className="user-data">
+                    <div className="username">Username: {user.username}</div>
+                    <div className="user-role">Role: {user.role}</div>
+                    <div className="user-status">Status: {user.status}</div>
+                </div>
+                <div className="user-actions-info">
+                    <div className="user-transactions">
+                        <Link className="user-link" to={{
+                            pathname: `${user.id}/transactions`,
+                            state: user.transactions
+                        }}>Transactions</Link>
+                    </div>
+                    <div className="user-files">
+                        <Link className="user-link" to={{
+                            pathname: `${user.id}/files`,
+                            state: user.files
+                        }}>Files</Link>
+                    </div>
+                    <div className="user-errors">
+                        <Link className="user-link" to={{
+                            pathname: `${user.id}/errors`,
+                            state: user.errors
+                        }}>Errors</Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
